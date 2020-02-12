eee7ff170c0695a74dbee40ee546310cf963c7ab
yiliang114 <1204183885@qq.com>
2020-02-12 12:59:12 +0800

    feat: remove components to theme

---
+++ example/.vuepress/theme/components/NavLink.vue
@@ -0,0 +1,50 @@
+<!--
+ * @Author: mrjzhang
+ * @Date: 2020-02-12 11:53:09
+ * @LastEditors: mrjzhang
+ * @LastEditTime: 2020-02-12 12:16:04
+ -->
+<template>
+  <router-link class="nav-link"
+    :to="link"
+    v-if="!isExternal(link)"
+    :exact="exact">{{ item.text }}</router-link>
+  <a v-else
+    :href="link"
+    class="nav-link external"
+    :target="isMailto(link) || isTel(link) ? null : '_blank'"
+    :rel="isMailto(link) || isTel(link) ? null : 'noopener noreferrer'">
+    {{ item.text }}
+    <OutboundLink />
+  </a>
+</template>
+
+<script>
+import { isExternal, isMailto, isTel, ensureExt } from "../utils";
+
+export default {
+  props: {
+    item: {
+      required: true
+    }
+  },
+  computed: {
+    link() {
+      return ensureExt(this.item.link);
+    },
+    exact() {
+      if (this.$site.locales) {
+        return Object.keys(this.$site.locales).some(
+          rootLink => rootLink === this.link
+        );
+      }
+      return this.link === "/";
+    }
+  },
+  methods: {
+    isExternal,
+    isMailto,
+    isTel
+  }
+};
+</script>
